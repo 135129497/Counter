14364cff30519e22ca7187c8d6b4d23ce30f481c
wangxiaohui <1351292497@qq.com>
2020-10-12 20:54:43 +0800

    修改文件

---
+++ 测试/ConsoleApplication2_5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1, s2;
+            char cha;
+            s1 = Convert.ToString(Console.ReadLine());
+            s2 = Convert.ToString(Console.ReadLine());
+            cha = Convert.ToChar(Console.ReadLine());
+            Counter c1 = new Counter();
+            c1.Count(s1,s2,cha);
+        }
+    }
+}
